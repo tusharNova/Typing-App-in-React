df93c2c5122fceb392e2528fb85e45274715bb71
AP_PC <tusharmanker02@gmail.com>
2023-11-24 20:01:12 +0530

    Add layout together

--- src/components/TypingLayout.jsx
+++ src/components/TypingLayout.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 export default class TypingLayout extends Component {
+        constructor(props){
+            super(props)
+        }
 
-
+        
 
     render() {
-        
+        const text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia maiores nihil quasi minima sit alias natus assumenda libero, neque rem sapiente, quaerat necessitatibus! Aliquid optio molestias, obcaecati commodi dolore non?"
+        const len = text.length
         return (
-            <div className="typinglayout">
-                <div ></div>
+            <>
+            <div className="wordlayout">
+                <p>{text}{len}</p>
+            </div>
+             <div className="typinglayout" >
+                <div contentEditable='true'  onInput={e => console.log('Text inside div', e.currentTarget.textContent)}></div>
             </div>
 
+            </>
+           
         )
     }
 }
--- src/components/Wordlayout.jsx
+++ src/components/Wordlayout.jsx
@@ -3,9 +3,9 @@ import React , {Component} from 'react'
 export default class Wordlayout extends Component{
     render(){
         return(
-            <div className="wordlayout">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos obcaecati veniam nobis iure inventore laudantium accusamus nihil quibusdam neque assumenda unde quis consequuntur sit, velit necessitatibus hic, aspernatur veritatis asperiores. Consequuntur odio ad suscipit reprehenderit minus earum, ab molestias incidunt velit assumenda doloremque dolorum dolores, amet culpa, commodi doloribus temporibus dicta laudantium atque. Accusamus ratione eos eveniet optio commodi architecto ipsum. Eaque, officiis rerum tempore odit provident ut, atque animi error unde officia ab laborum assumenda. Ea dicta quas deserunt, nihil magnam libero odit et, consequuntur sapiente exercitationem, repellendus tempora vitae facilis qui aliquam porro dolorem nostrum. Obcaecati, qui labore.</p>
-            </div>
+            <>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia maiores nihil quasi minima sit alias natus assumenda libero, neque rem sapiente, quaerat necessitatibus! Aliquid optio molestias, obcaecati commodi dolore non?
+            </>
         )
     }
 }
